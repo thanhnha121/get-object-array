63d70ac600ff4f2582312acba3445ff27e77a99c
Nha Hoang <nhaht94@gmail.com>
2018-03-23 16:04:07 +0700

    nhaht update

--- get-object-array.js
+++ get-object-array.js
@@ -10,9 +10,9 @@ function GOA_DetectArray(data, array) {
             } catch (e) {
                 isArray = false;
             }
-            if (!isNaN(array[i][j]) && isArray) {
+            if (isArray) {
                 var left = GOA_SplitLeftArray(array[i], j, data);
-                var right = GOA_SplitRightArray(array[i], j); 
+                var right = GOA_SplitRightArray(data, array[i], j); 
                 result = GOA_AddToResult(data, result, array, left, right);
             }
         }
@@ -23,38 +23,54 @@ function GOA_DetectArray(data, array) {
 function GOA_AddToResult(data, result, array, left, right) {
     var check = 0;
     for (var i = result.length - 1; i >= 0; i--) {
-        if (result[i].path == left) {
+        if (result[i].path === left) {
             var fieldsString = result[i].fields.map(x => x.name);
-            if (!fieldsString.includes(right)) {
-                result[i].fields.push({
-                    name: right,
-                    value: eval('data.' + left + '[0].' + right) 
-                });
-            }
+            try {
+                if (!fieldsString.includes(right)) {
+                    result[i].fields.push({
+                        name: right,
+                        value: eval('data.' + left + '[0].' + right) 
+                    });
+                }
+            } catch (e) {}
+            
             check = 1;
         }
     }
     if (check === 0) {
-        result.push({
-            path: left,
-            fields: [ {
-                name: right,
-                value: eval('data.' + left + '[0].' + right) 
-            } ]
-        });
+        try {
+            result.push({
+                path: left,
+                fields: [ {
+                    name: right,
+                    value: eval('data.' + left + '[0].' + right) 
+                } ]
+            });
+        } catch (e) {}
+        
     }
     return result;
 }
 
 
-function GOA_SplitRightArray(array, index) {
+function GOA_SplitRightArray(data, array, index) {
     var rs = '';
     for (var i = index + 1; i <= array.length - 1; i++) {
         if (i != index + 1 && isNaN(array[i])) {
             rs += '.';
         }
-        if (!isNaN(array[i])) {
+        var tmp = GOA_SplitLeftArray(array, i, data);
+        var isArray = false;
+        try {
+            isArray = Array.isArray(eval('data.' + tmp));
+        } catch (e) {
+            isArray = false;
+        }
+
+        if (!isNaN(array[i]) && isArray) {
             rs += '[' + array[i] + ']';
+        } else if (!isNaN(array[i]) && !isArray) {
+            rs += '[\'' + array[i] + '\']';
         } else {
             rs += array[i];
         }
@@ -65,10 +81,9 @@ function GOA_SplitRightArray(array, index) {
 function GOA_SplitLeftArray(array, index, data) {
     var rs = '';
     for (var i = 0; i <= index - 1; i++) {
-        var tmp = array.slice(0, i - 1).join('.');
         var isArray = false;
         try {
-            isArray = Array.isArray(eval('data.' + tmp));
+            isArray = Array.isArray(eval('data.' + rs));
         } catch (e) {
             isArray = false;
         }
@@ -93,7 +108,7 @@ function GOA_ObjTree(item) {
         var keys = Object.keys(r);
         if (keys.length) {
             return keys.forEach((x) => {
-                if (typeof r[x] == 'object') {
+                if (typeof r[x] === 'object') {
                     iter(r[x], p.concat(x));
                 } else {
                     result.push(p.concat(x));
@@ -114,23 +129,42 @@ function GOA_Execute(data) {
 }
 
 function GOA_Demo() {
-    var data = {"menu": {
-        "header": "SVG Viewer",
-        "items": [
-            {"id": "OpenNew", "label": "Open New"},
-            {"id": "ZoomIn", "label": "Zoom In"},
-            {"id": "ZoomOut", "label": "Zoom Out"},
-            {"id": "OriginalView", "label": "Original View"},
-            {"id": "Find", "label": "Find..."},
-            {"id": "FindAgain", "label": "Find Again"},
-            {"id": "CopyAgain", "label": "Copy Again"},
-            {"id": "CopySVG", "label": "Copy SVG"},
-            {"id": "ViewSVG", "label": "View SVG"},
-            {"id": "ViewSource", "label": "View Source"},
-            {"id": "SaveAs", "label": "Save As"},
-            {"id": "About", "label": "About Adobe CVG Viewer..."}
-        ]
-    }};
+    var data = {
+            "menu": {
+            "header": "SVG Viewer",
+            "items": [
+                {"id": "OpenNew", "label": "Open New"},
+                {"id": "ZoomIn", "label": "Zoom In"},
+                {"id": "ZoomOut", "label": "Zoom Out"},
+                {"id": "OriginalView", "label": "Original View"},
+                {"id": "Find", "label": "Find..."},
+                {"id": "FindAgain", "label": "Find Again"},
+                {"id": "CopyAgain", "label": "Copy Again"},
+                {"id": "CopySVG", "label": "Copy SVG"},
+                {"id": "ViewSVG", "label": "View SVG"},
+                {"id": "ViewSource", "label": "View Source"},
+                {"id": "SaveAs", "label": "Save As"},
+                {"id": "About", "label": "About Adobe CVG Viewer..."}
+            ],
+            "more_info": [ 
+                { "language": "EN", "locate": "US" },
+                {
+                    0: { name: "John", age: 20 },
+                    1: { 
+                        name: "Deo", 
+                        age: 22,
+                        objects: [ { name: "Square", color: "Red" } ]
+                    }
+                },
+                {
+                    tags: [
+                        { id: 1, tag: "Deeper" },
+                        { id: 2, tag: "Slap" }
+                    ]
+                }
+            ]
+        }
+    };
 
     var result = GOA_Execute(data);
     console.log('Result: ', result);
